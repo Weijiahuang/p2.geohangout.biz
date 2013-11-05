425bb2e5f31ca3bae71b1c9bb9cdcb2a653d1191
Weijia Huang <hhuangweijia@gmail.com>
2013-11-04 19:46:15 -0500

    Third commit

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -9,6 +9,7 @@ class users_controller extends base_controller {
         echo "This is the index page";
     }
 
+ 	# Check duplicate email
     public function signup($uniqueness=NULL) {
         
         # Set up the view
@@ -20,6 +21,7 @@ class users_controller extends base_controller {
         echo $this->template;
     }
     
+   	# check whether all the fields are filled
     public function check($blankness=NULL) {
         
         # Set up the view
@@ -85,8 +87,9 @@ class users_controller extends base_controller {
     }
     
     
-    public function login($error=NULL) {    
-        #
+    public function login($error=NULL) 
+    {    
+        # set up the view
         $this->template-> content = View::instance('v_users_login');
         
         $this->template->title="Login";
@@ -101,71 +104,69 @@ class users_controller extends base_controller {
 	public function p_login()
 	{
 
-    # Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
-    $_POST = DB::instance(DB_NAME)->sanitize($_POST);
+    	# Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
+    	$_POST = DB::instance(DB_NAME)->sanitize($_POST);
 
-    # Hash submitted password so we can compare it against one in the db
-    $_POST['password1'] = sha1(PASSWORD_SALT.$_POST['password1']);
+    	# Hash submitted password so we can compare it against one in the db
+    	$_POST['password1'] = sha1(PASSWORD_SALT.$_POST['password1']);
 	
 	
-    # Search the db for this email and password
-    # Retrieve the token if it's available
-    $q = "SELECT token 
-        FROM users 
-        WHERE email = '".$_POST['email1']."' 
-        AND password = '".$_POST['password1']."'";
+    	# Search the db for this email and password
+    	# Retrieve the token if it's available
+    	$q = "SELECT token 
+        	FROM users 
+        	WHERE email = '".$_POST['email1']."' 
+        	AND password = '".$_POST['password1']."'";
 
-    $token = DB::instance(DB_NAME)->select_field($q);
+    	$token = DB::instance(DB_NAME)->select_field($q);
 	
-    # If we found a matching token in the database, it means login successful
-    if($token) 
-	{
-        /* 
-        Store this token in a cookie using setcookie()
-        Important Note: *Nothing* else can echo to the page before setcookie is called
-        Not even one single white space.
-        param 1 = name of the cookie
-        param 2 = the value of the cookie
-        param 3 = when to expire
-        param 4 = the path of the cooke (a single forward slash sets it for the entire domain)
-        */
-        setcookie("token", $token, strtotime('+1 year'), '/');
+    	# If we found a matching token in the database, it means login successful
+    	if($token) 
+		{
+        	/* 
+        	Store this token in a cookie using setcookie()
+        	Important Note: *Nothing* else can echo to the page before setcookie is called
+        	Not even one single white space.
+        	param 1 = name of the cookie
+        	param 2 = the value of the cookie
+        	param 3 = when to expire
+        	param 4 = the path of the cooke (a single forward slash sets it for the entire domain)
+        	*/
+        	setcookie("token", $token, strtotime('+1 year'), '/');
 		
-        # Send them to the main page - or whever you want them to go
-        Router::redirect("/posts/index");
-    }
-    
-    else
-	{
-        # Send them back to the login page
-        Router::redirect("/users/login/error");
-    }
-}
+        	# Send them to the main page - or whever you want them to go
+        	Router::redirect("/posts/index");
+    	}
+    	# If login fail, pass variable $error to login page
+    	else
+		{
+        	# Send them back to the login page
+        	Router::redirect("/users/login/error");
+    	}
+	}
 
     public function logout() 
     {
-    
-    # Generate and save a new token for next login
-    $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+    	# Generate and save a new token for next login
+    	$new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
 
-    # Create the data array we'll use with the update method
-    # In this case, we're only updating one field, so our array only has one entry
-    $data = Array("token" => $new_token);
+    	# Create the data array we'll use with the update method
+    	# In this case, we're only updating one field, so our array only has one entry
+    	$data = Array("token" => $new_token);
 
-    # Do the update
-    DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+    	# Do the update
+    	DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
 
-    # Delete their token cookie by setting it to a date in the past - effectively logging them out
-    setcookie("token", "", strtotime('-1 year'), '/');
+    	# Delete their token cookie by setting it to a date in the past - effectively logging them out
+    	setcookie("token", "", strtotime('-1 year'), '/');
 
-    # Send them back to the main index.
-    Router::redirect("/");
+    	# Send them back to the main index.
+    	Router::redirect("/");
     }  
-    
 
-    public function profile($user_name = NULL) {
-  
-    	# check whether the users is loged in or not
+    public function profile($user_name = NULL) 
+    {
+  		# check whether the users is loged in or not
     	if(!$this->user)
     	{
     		echo "Members only";
